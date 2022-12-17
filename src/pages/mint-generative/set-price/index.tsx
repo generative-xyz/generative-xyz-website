aa17bd150890878ce38ebea39ac35c14e87be836
Archaon <ducanh@myconstant.com>
2022-12-17 15:54:49 +0700

    chore: add mint generative context to _app

--- src/pages/mint-generative/set-price/index.tsx
+++ src/pages/mint-generative/set-price/index.tsx
@@ -1,15 +1,12 @@
 import DefaultLayout from '@components/Layout/DefaultLayout';
 import MintGenerative from '@containers/MintGenerative';
 import MintGenerativeStep3 from '@containers/MintGenerative/Step3';
-import { MintGenerativeContextProvider } from '@contexts/mint-generative-context';
 import { NextPage } from 'next';
 
 const MintGenerativePage: NextPage = () => {
   return (
     <DefaultLayout>
-      <MintGenerativeContextProvider>
-        <MintGenerative pageRender={<MintGenerativeStep3 />} />
-      </MintGenerativeContextProvider>
+      <MintGenerative pageRender={<MintGenerativeStep3 />} />
     </DefaultLayout>
   );
 };
--- src/pages/mint-generative/upload-project/index.tsx
+++ src/pages/mint-generative/upload-project/index.tsx
@@ -1,15 +1,12 @@
 import DefaultLayout from '@components/Layout/DefaultLayout';
 import MintGenerative from '@containers/MintGenerative';
 import MintGenerativeStep1 from '@containers/MintGenerative/Step1';
-import { MintGenerativeContextProvider } from '@contexts/mint-generative-context';
 import { NextPage } from 'next';
 
 const MintGenerativePage: NextPage = () => {
   return (
     <DefaultLayout>
-      <MintGenerativeContextProvider>
-        <MintGenerative pageRender={<MintGenerativeStep1 />} />
-      </MintGenerativeContextProvider>
+      <MintGenerative pageRender={<MintGenerativeStep1 />} />
     </DefaultLayout>
   );
 };
