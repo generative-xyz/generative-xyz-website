6e3dbfa8b5cf28812651964af4fa4ceceef7b1fd
Archaon <ducanh@myconstant.com>
2022-12-23 10:42:13 +0700

    style: update css

--- src/components/Collection/Item/index.tsx
+++ src/components/Collection/Item/index.tsx
@@ -10,10 +10,10 @@ const CollectionItem = ({ data }: { data: IProjectItem }) => {
     <div className={styles.wrapper}>
       <div className={styles.thumbnail}>
         <Image
-          src={data.image}
+          src={data?.image || ''}
           alt="item thumbnail"
           fill
-          style={{ objectFit: 'cover', width: '100%' }}
+          style={{ width: '100%' }}
           sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 25vw"
         />
@@ -29,7 +29,7 @@ const CollectionItem = ({ data }: { data: IProjectItem }) => {
         />
       </Stack>
       <div className={styles.info}>
-        <b>price ETH</b>
+        <b>0.2 ETH</b>
         <Stack direction="horizontal">
           <span>by &nbsp;</span>
           <AvatarInfo imgSrc="" width={32} height={32} />
