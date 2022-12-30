4beb5bdde56182353fb0c9215269a00f96fdd284
Duc Anh Nguyen <ducanh@myconstant.com>
2022-12-30 10:12:36 +0700

    feat: header wip

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,5 @@
+import { CDN_URL } from './config';
+
 export const MENUS: Array<{ title: string; url: string }> = [
   // {
   //   title: 'create',
@@ -19,6 +21,8 @@ export const MENUS: Array<{ title: string; url: string }> = [
 
 export const LOGO_URL =
   'https://cdn.autonomous.ai/static/upload/images/common/upload/20221221/logo6cbc3e712f.svg';
+
+export const LOGO_MARKETPLACE_URL = `${CDN_URL}/icons/logo-marketplace.svg`;
 
 export const CLOUDFRONT_SERVER_NAME = 'https://cdn.autonomous.ai/';
 export const PLACE_HOLDER_IMAGE = `${CLOUDFRONT_SERVER_NAME}static/upload/images/common/upload/20210519/2560x1600-dark-gray-solid-color-backgrounda218631f0e.jpeg`;
--- src/layouts/Marketplace/Header.tsx
+++ src/layouts/Marketplace/Header.tsx
@@ -10,12 +10,22 @@ import { LogLevel } from '@enums/log-level';
 import { useAppSelector } from '@redux';
 import { getUserSelector } from '@redux/user/selector';
 import { formatAddress } from '@utils/format';
+import SvgInset from '@components/SvgInset';
+import { LOGO_MARKETPLACE_URL } from '@constants/common';
+import { useRouter } from 'next/router';
+import cs from 'classnames';
 
-const LOG_PREFIX = 'Header';
+const LOG_PREFIX = 'MarketplaceHeader';
+
+const MENU_HEADER = [
+  { id: 1, name: 'create', route: ROUTE_PATH.CREATE_PROJECT },
+  { id: 2, name: 'projects', route: ROUTE_PATH.GENERATIVE },
+];
 
 const Header: React.FC = (): React.ReactElement => {
   const walletCtx = useContext(WalletContext);
   const user = useAppSelector(getUserSelector);
+  const router = useRouter();
 
   const handleConnectWallet = async (): Promise<void> => {
     try {
@@ -32,15 +42,23 @@ const Header: React.FC = (): React.ReactElement => {
           <div className="d-flex justify-content-between w-100">
             <Stack direction="horizontal">
               <h1>
-                <Link href={ROUTE_PATH.HOME}>G</Link>
+                <Link href={ROUTE_PATH.HOME}>
+                  <SvgInset
+                    svgUrl={LOGO_MARKETPLACE_URL}
+                    className={styles.logo}
+                  />
+                </Link>
               </h1>
               <ul className={styles.navBar}>
-                <li>
-                  <Link href={ROUTE_PATH.CREATE_PROJECT}>Create</Link>
-                </li>
-                <li>
-                  <Link href={ROUTE_PATH.GENERATIVE}>Projects</Link>
-                </li>
+                {MENU_HEADER?.length > 0 &&
+                  MENU_HEADER.map(item => (
+                    <li
+                      className={cs(router.pathname === item.route)}
+                      key={`header-${item.id}`}
+                    >
+                      <Link href={item.route}>{item.name}</Link>
+                    </li>
+                  ))}
               </ul>
             </Stack>
             {user.id ? (
