03106e835b051286172ecd172de577e8fb8eaf45
benjiro2077 <nhan.nguyen@autonomous.nyc>
2023-01-03 13:33:15 +0700

    chore: update seo info, manifest.json

--- src/constants/seo-default-info.ts
+++ src/constants/seo-default-info.ts
@@ -1,4 +1,4 @@
-export const SEO_TITLE = 'Generative.';
-export const SEO_DESCRIPTION = 'Breathe life into your generative art.';
+export const SEO_TITLE = 'Generative';
+export const SEO_DESCRIPTION = 'Breathe life into your generative art';
 export const SEO_IMAGE =
   'https://cdn.generative.xyz/pages/home/screen-shot.jpeg';
