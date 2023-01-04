0e7efa352e1542b4a0a8e9aa201aaf0578bc1884
Duc Anh Nguyen <ducanh@myconstant.com>
2023-01-04 16:07:54 +0700

    feat: revert code

--- src/containers/GenerativeTokenDetail/index.tsx
+++ src/containers/GenerativeTokenDetail/index.tsx
@@ -23,8 +23,6 @@ import Link from 'next/link';
 
 const LOG_PREFIX = 'GenerativeTokenDetail';
 
-const MARKETPLACE_API = false; // TODO: wait for Marketplace API
-
 const GenerativeTokenDetail: React.FC = (): React.ReactElement => {
   const router = useRouter();
   const { tokenID } = router.query as {
