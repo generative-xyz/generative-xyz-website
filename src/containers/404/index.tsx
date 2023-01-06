b49adc416ecd7ae26f8f5f4bca23edcdc95010c9
Dev Hon Tran <hon@automonous.ai>
2023-01-06 11:09:16 +0700

    feat: done 404 page

--- src/containers/404/index.tsx
+++ src/containers/404/index.tsx
@@ -11,6 +11,7 @@ import { LogLevel } from '@enums/log-level';
 import { WalletContext } from '@contexts/wallet-context';
 import { useRouter } from 'next/router';
 import { ROUTE_PATH } from '@constants/route-path';
+import { AnimFade } from '@animations/fade';
 const LOG_PREFIX = 'Marketplace404';
 
 export const Page404 = (): JSX.Element => {
@@ -50,32 +51,37 @@ export const Page404 = (): JSX.Element => {
       <div className={s.error404_inner}>
         <Heading
           as="h1"
+          className={s.error404_heading}
           animOption={{ screen: 0.1, offset: 0, type: 'heading' }}
         >
           Almost here
         </Heading>
         <Heading
           as="h5"
-          animOption={{ screen: 0.1, offset: 0, type: 'random' }}
+          animOption={{ screen: 0.3, offset: 0, type: 'random' }}
         >
           To continue you need to connect your wallet.
         </Heading>
         <ul className={s.error404_ctas}>
           {!user.id && (
             <li>
-              <ButtonIcon
-                sizes="medium"
-                variants={'primary'}
-                onClick={handleConnectWallet}
-              >
-                Connect wallet
-              </ButtonIcon>
+              <AnimFade screen={0.5}>
+                <ButtonIcon
+                  sizes="medium"
+                  variants={'primary'}
+                  onClick={handleConnectWallet}
+                >
+                  Connect wallet
+                </ButtonIcon>
+              </AnimFade>
             </li>
           )}
           <li>
-            <ButtonIcon sizes="medium" variants={'outline'} onClick={goHome}>
-              Take me home
-            </ButtonIcon>
+            <AnimFade screen={0.6}>
+              <ButtonIcon sizes="medium" variants={'outline'} onClick={goHome}>
+                Take me home
+              </ButtonIcon>
+            </AnimFade>
           </li>
         </ul>
       </div>
