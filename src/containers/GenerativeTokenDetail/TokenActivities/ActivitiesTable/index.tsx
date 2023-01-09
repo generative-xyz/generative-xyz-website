012400c798e216f0febd570b85f961f5c9d4e1ce
Duc Anh Nguyen <ducanh@myconstant.com>
2023-01-09 17:21:58 +0700

    feat: add image when no item found

---
+++ src/containers/GenerativeTokenDetail/TokenActivities/ActivitiesTable/index.tsx
@@ -0,0 +1,8 @@
+import NotFound from '@components/NotFound';
+import React from 'react';
+
+const TableActivities = () => {
+  return <NotFound infoText="No activity yet" />;
+};
+
+export default TableActivities;
