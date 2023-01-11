398dd5258c545a9a33e96c8a874da80e6600aad3
benjiro2077 <nhan.nguyen@autonomous.nyc>
2023-01-11 15:59:18 +0700

    feat: make display animation mode as default mode

--- src/containers/MintGenerative/ProjectDetail/index.tsx
+++ src/containers/MintGenerative/ProjectDetail/index.tsx
@@ -11,6 +11,7 @@ import { getCategoryList } from '@services/category';
 import { MintGenerativeContext } from '@contexts/mint-generative-context';
 import { useRouter } from 'next/router';
 import { SelectOption } from '@interfaces/select-input';
+import { THIRD_PARTY_SCRIPTS } from '@constants/mint-generative';
 
 type IProductDetailFormValue = {
   name: string;
@@ -20,33 +21,6 @@ type IProductDetailFormValue = {
   tags: Array<string>;
   thirdPartyScripts: Array<SelectOption>;
 };
-
-const THIRD_PARTY_SCRIPTS = [
-  {
-    label: 'p5js@1.5.0',
-    value: 'p5js@1.5.0',
-  },
-  {
-    label: 'threejs@r124',
-    value: 'threejs@r124',
-  },
-  {
-    label: 'tonejs@14.8.49',
-    value: 'tonejs@14.8.49',
-  },
-  {
-    label: 'c2.min.js@1.0.0',
-    value: 'c2.min.js@1.0.0',
-  },
-  {
-    label: 'chromajs@2.4.2',
-    value: 'chromajs@2.4.2',
-  },
-  {
-    label: 'p5.grain.js@0.6.1',
-    value: 'p5.grain.js@0.6.1',
-  },
-];
 
 const ProjectDetail: React.FC = (): React.ReactElement => {
   const router = useRouter();
